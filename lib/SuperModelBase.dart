0de4c5eb29b63482715286e5db750a152b140d50
Gary McGhee <gary@huey.co>
2025-04-02 17:39:53 +0800

    removed GetProperty

--- lib/SuperModelBase.dart
+++ lib/SuperModelBase.dart
@@ -3,14 +3,5 @@ part of './super_model.dart';
 abstract class SuperModelBase {
   ModelClassMeta get $classMeta => ModelClassMeta(SuperModelBase,null,null,{});
 
-  Map<String, dynamic Function()> get $getters => Map<String, dynamic Function()>.unmodifiable({});
-
-  //const SuperModelBase();
-
-  T getProperty<T>(String propertyName) {
-    var getter = $getters[propertyName];
-    if (getter==null)
-      throw new ArgumentError("property $propertyName doesn't exist");
-    return getter() as T;
-  }
+  Map<String, dynamic Function(SuperModelBase)> get $getters => Map<String, dynamic Function(SuperModelBase)>.unmodifiable({});
 }
--- test/AssociationAnimal.dart
+++ test/AssociationAnimal.dart
@@ -15,16 +15,6 @@ class Person extends SuperModelBase with PersonMappable, PersonMeta, PersonMappa
   @SuperModelId()
   final int id;
   final String name;
-  
-  // Implementation of required method from SuperModelBase
-  @override
-  T getProperty<T>(String propertyName) {
-    switch (propertyName) {
-      case 'id': return id as T;
-      case 'name': return name as T;
-      default: throw Exception('Unknown property: $propertyName');
-    }
-  }
 }
 
 // an animal model using full dart_mappable generation and implementing ISuperModel using @MappableSuperModel() macro
@@ -45,36 +35,23 @@ class AssociationAnimal extends SuperModelBase with AssociationAnimalMappable, A
   final String name;
   final String? species;
   final int? age;
-  
+
   @BelongsTo(foreignKey: 'animalId')
   Person? owner;
-  
-  // Implementation of required method from SuperModelBase
-  @override
-  T getProperty<T>(String propertyName) {
-    switch (propertyName) {
-      case 'id': return id as T;
-      case 'name': return name as T;
-      case 'species': return species as T;
-      case 'age': return age as T;
-      case 'owner': return owner as T;
-      default: throw Exception('Unknown property: $propertyName');
-    }
-  }
-  
+
   // Add static serialization methods needed for tests
   static AssociationAnimal fromJson(String json) => AssociationAnimalMapper.fromJson(json);
   static AssociationAnimal fromMap(Map<String, dynamic> map) => AssociationAnimalMapper.fromMap(map);
   static AssociationAnimal $fromJson(String json) => AssociationAnimalMapper.fromJson(json);
   static AssociationAnimal $fromMap(Map<String, dynamic> map) => AssociationAnimalMapper.fromMap(map);
-  
+
   // Add static field name constants for tests
   static const String $name = 'name';
   static const String $species = 'species';
   static const String $age = 'age';
   static const String $id = 'id';
   static const String $owner = 'owner';
-  
+
   // Add static meta for tests
   static const ModelClassMeta $meta = ModelClassMeta(AssociationAnimal, 'id', int, {
     $id: PropertyMeta($id, int, false, 'int', 'int'),
